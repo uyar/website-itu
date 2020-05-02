ffc81f66e56826d1aafaaa097b53596036b8ea86
H. Turgut Uyar <uyar@itu.edu.tr>
2020-05-02 18:11:45 +0300

    start custom theme

--- conf.py
+++ conf.py
@@ -155,7 +155,7 @@ NAVIGATION_ALT_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "bootblog4"
+THEME = "uyar_itu"
 
 # Primary color of your theme. This will be used to customize your theme.
 # Must be a HEX value.
@@ -1296,7 +1296,7 @@ COPY_SOURCES = False
 # Bundle JS and CSS into single files to make site loading faster in a HTTP/1.1
 # environment but is not recommended for HTTP/2.0 when caching is used.
 # Defaults to True.
-# USE_BUNDLES = True
+USE_BUNDLES = False
 
 # Plugins you don't want to use. Be careful :-)
 # DISABLED_PLUGINS = ["render_galleries"]
