66d018645acf235438dd567a9dd5db865af420e8
H. Turgut Uyar <uyar@itu.edu.tr>
2020-05-10 03:34:52 +0300

    switch to node-sass

--- conf.py
+++ conf.py
@@ -1364,7 +1364,7 @@ GLOBAL_CONTEXT = {}
 GLOBAL_CONTEXT_FILLER = []
 
 # Compiler to process Sass files.
-SASS_COMPILER = 'sass'
+SASS_COMPILER = './node_modules/.bin/node-sass'
 
 # A list of options to pass to the Sass compiler.
 # Final command is: SASS_COMPILER SASS_OPTIONS file.s(a|c)ss
