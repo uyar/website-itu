91dad667ae128fa5217990c01e8364294168bb43
H. Turgut Uyar <uyar@itu.edu.tr>
2020-05-10 16:42:52 +0300

    add rsync deploy setting

--- conf.py
+++ conf.py
@@ -647,11 +647,11 @@ REDIRECTIONS = []
 # to `nikola deploy`.  If no arguments are specified, a preset
 # named `default` will be executed.  You can use as many presets
 # in a `nikola deploy` command as you like.
-# DEPLOY_COMMANDS = {
-#     'default': [
-#         "rsync -rav --delete output/ joe@my.site:/srv/www/site",
-#     ]
-# }
+DEPLOY_COMMANDS = {
+    'default': [
+        "rsync -rav --delete output/ uyar@ssh.itu.edu.tr:web.itu.edu.tr/public_html/",
+    ]
+}
 
 # github_deploy configuration
 # For more details, read the manual:
